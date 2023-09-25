b554905dcee7c638ac01a2a43bd9f90e6099686c
Gilvan Almeida <gilvan.almeida@version1.com>
2023-09-25 17:28:31 +0100

    Insert Intervals

---
+++ Insert_intervals.py
@@ -0,0 +1,46 @@
+import unittest
+
+# You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+# Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+# Return intervals after the insertion.
+
+# Example 1:
+
+# Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+# Output: [[1,5],[6,9]]
+
+# Example 2:
+
+# Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+# Output: [[1,2],[3,10],[12,16]]
+# Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+
+#Time complexity O(n log n) : Space complexity O(n)
+def insert_intervals(intervals, new_interval):
+    intervals.append(new_interval)
+    intervals.sort(key = lambda i : i[0])
+    output = [intervals[0]]
+    for start, end in intervals[1:]:
+        last_end = output[-1][1]
+        if start <= last_end:
+            output[-1][1] = max(last_end, end)
+        else:
+            output.append([start, end])
+    return output
+
+class Test(unittest.TestCase):
+    test_cases = [
+        ([[1,3], [6,9]], [2,5], [[1,5],[6,9]]),
+        ([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8], [[1,2],[3,10],[12,16]])
+    ]
+    functions = [insert_intervals]
+    def test_insert_intervals(self):
+        for function in self.functions:
+            for arr, new_interval, expected in self.test_cases:
+                result = function(arr, new_interval)
+                assert result == expected
+
+if __name__ == '__main__':
+    unittest.main()
