ed905a96b602a4a22890d1ef2600ee6db1da05c1
Gilvan Almeida <gilvan.almeida@version1.com>
2023-12-11 15:28:36 +0000

    Reverse

---
+++ reverse_bits.py
@@ -0,0 +1,26 @@
+import unittest
+
+#TC: O(n)
+#EC: O(1)
+def reverse_bit(n):
+    result = 0
+    bit_length = 32
+    for i in range(bit_length):
+        bit = (n >> i) & 1
+        result |= bit << (bit_length - 1 - i)
+    return result
+
+class Test(unittest.TestCase):
+    test_cases = [
+        (0b00000010100101000001111010011100, 964176192 ),
+        (0b11111111111111111111111111111101, 3221225471 ),
+    ]
+    functions = [reverse_bit]
+    def test_reverse_bit(self):
+        for function in self.functions:
+            for n, expected in self.test_cases:
+                result = function(n)
+                assert result == expected
+
+if __name__ == '__main__':
+    unittest.main()
