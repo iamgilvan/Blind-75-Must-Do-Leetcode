09f5cc28a06ca984b8c9ffaca758d29535fbc18f
Gilvan Almeida <gilvan.almeida@version1.com>
2024-04-13 19:15:17 +0100

    Contains Duplicates : new method

--- Contains_Duplicate.py
+++ Contains_Duplicate.py
@@ -1,6 +1,6 @@
 import unittest
 
-#TC O(n)
+#TC O(nlogn)
 #TS O(1)
 def contains_duplicates(arr):
     arr.sort()
@@ -9,13 +9,23 @@ def contains_duplicates(arr):
             return True
     return False
 
+#TC O(n)
+#TS O(n)
+def contains_duplicates_hashset(arr):
+    hashset = set()
+    for i in arr:
+        if i in hashset:
+            return True
+        hashset.add(i)
+    return False
+
 class Test(unittest.TestCase):
     test_cases = [
         ([1,2,3,1], True),
         ([1,2,3,4], False),
         ([1,1,1,3,3,4,3,2,4,2], True)
     ]
-    functions = [contains_duplicates]
+    functions = [contains_duplicates, contains_duplicates_hashset]
     def test_contains_duplicates(self):
         for function in self.functions:
             for arr, expected in self.test_cases:
