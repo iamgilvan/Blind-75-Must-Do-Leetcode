8c4992970117e730216544f15a5cb737599b4fd3
Gilvan Almeida <gilvan.almeida@version1.com>
2024-08-20 17:05:33 +0100

    Kth Largest Element in an Stream

--- Construct Binary_Tree_from_Preorder_and_Inorder_Traversal.py
+++ Construct Binary_Tree_from_Preorder_and_Inorder_Traversal.py
@@ -1,12 +1,13 @@
 import unittest
 
+# Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
 #Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-
+# O(N^2)
 def buildTree(preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
     if not preorder or not inorder:
         return None
