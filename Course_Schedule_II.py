153888040b1150f2c21f9898c0528b9527920947
Gilvan Almeida <gilvan.almeida@version1.com>
2024-10-04 14:20:00 +0100

    Course Schedule II

--- Course_Schedule_II.py
+++ Course_Schedule_II.py
@@ -5,39 +5,41 @@ import unittest
 
 # Return true if you can finish all courses. Otherwise, return false.
 #TC O(V + E), #EC O(V + E)  V = numCourses E = prerequisites
-def course_schedule(numCourses, prerequisites):
+def course_schedule_ii(numCourses, prerequisites):
     graph = [[] for _ in range(numCourses)]  # graph
     visited = [0] * numCourses  # 0: not visited, 1: visiting, 2: visited
 
     # build the graph
     for course, prerequisite in prerequisites:
         graph[course].append(prerequisite)
-
+    res = []
     def has_cycle(course):
         if visited[course] == 1: #has cycle
-            return True
-        if visited[course] == 2: #visited
             return False
+        if visited[course] == 2: #visited
+            return True
         visited[course] = 1 # marked as visiting
 
         for preReq in graph[course]:
-            if has_cycle(preReq):
-                return True
+            if not has_cycle(preReq):
+                return False
         visited[course] = 2 # marked as visited
-        return False
+        res.append(course)
+        return True
     # check if there is cycle in each course
     for i in range(numCourses):
-        if has_cycle(i):
-            return False
-    return True
+        if not has_cycle(i):
+            return []
+    return res
 class Test(unittest.TestCase):
     test_cases = [
-        (2, [[1,0]], True),
-        (2, [[1,0],[0,1]], False),
-        (3, [[1,0],[0,2],[2,1]], False)
+        (2, [[1,0]],  [0,1]),
+        (4, [[1,0],[2,0],[3,1],[3,2]], [0,1,2,3]),
+        (1, [], [0]),
+        (2, [[0,1]], [1,0])
     ]
-    functions = [course_schedule]
-    def test_course_schedule(self):
+    functions = [course_schedule_ii]
+    def test_course_schedule_ii(self):
         for function in self.functions:
             for courseN, arr, expected in self.test_cases:
                 result = function(courseN, arr)
