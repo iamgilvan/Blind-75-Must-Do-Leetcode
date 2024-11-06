b465003c58018e0984f3a77700fbc9083b3084bd
Gilvan Almeida <gilvan.almeida@version1.com>
2024-11-06 15:26:30 +0000

    Maximum Product Subarray

--- Maximum_Product_Subarray.py
+++ Maximum_Product_Subarray.py
@@ -18,14 +18,25 @@ def maximum_product_subarray(arr):
 
     return max_p
 
-
+#TC O(n)
+#TS O(1)
+def maximum_product_subarray_iii(arr):
+    max_prod = -float('inf')
+    curr_max = curr_min = 1
+    for num in arr:
+        old_max = curr_max * num
+        curr_max = max(old_max, curr_min*num,num)
+        curr_min = min(old_max, curr_min*num,num)
+        max_prod = max(curr_max, max_prod)
+    return max_prod
 class Test(unittest.TestCase):
     test_cases = [
         ([2,3,-2,4],  6),
+        ([2,3,-2,4, -1],  48),
         ([-2,0,-1],  0),
         ([-1,-1],  1)
     ]
-    functions = [maximum_product_subarray]
+    functions = [maximum_product_subarray_iii, maximum_product_subarray]
     def test_maximum_product_subarray(self):
         for function in self.functions:
             for arr, expected in self.test_cases:
